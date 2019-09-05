8440ae052d36f0b9a1784a6692d1d843ff8df667
MariaGemini <maria.gemini@hotmail.com>
2019-09-04 21:42:50 -0300

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Person persona1 = new Person();
+        Teacher teacher1 = new Teacher();
+        Student estudiante1 = new Student();
+
+
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,20 @@
+public class Student extends Person {
+    private String enrollment;
+    private String career;
+
+    public String getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(String enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+}
